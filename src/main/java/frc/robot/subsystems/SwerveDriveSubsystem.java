99df563e1723a00c4714bfb155406d83ebb9dfeb
RealCreativit <335999@apps.everettsd.org>
2023-02-07 17:17:46 -0800

    Annotation

--- src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -7,6 +7,13 @@ import frc.robot.Functions;
 
 public class SwerveDriveSubsystem extends SubsystemBase {
     
+    //F = front
+    //R = right
+    //B = back
+    //L = left
+    //A = angle
+    //T = throttle
+
     double FRX;
     double FRY;
     double FLX;
@@ -56,7 +63,7 @@ public class SwerveDriveSubsystem extends SubsystemBase {
             LSY = Functions.Exponential(Functions.DeadZone(-Constants.controller.getLeftY(), Constants.controllerDeadZone));
             RSX = Functions.Exponential(Functions.DeadZone(Constants.controller.getRightX(), Constants.controllerDeadZone));
             
-
+            //set the target X and Y speeds based on controller input
             FRX = Constants.turnMultiplier*RSX + LSX;
             FRY = -Constants.turnMultiplier*RSX + LSY;
             FLX = Constants.turnMultiplier*RSX + LSX;
@@ -66,23 +73,26 @@ public class SwerveDriveSubsystem extends SubsystemBase {
             BLX = -Constants.turnMultiplier*RSX + LSX;
             BLY = Constants.turnMultiplier*RSX + LSY;
 
+            //set the target angles based on the target X and Y speeds
             FRA = Math.atan2(FRX, FRY);
             FLA = Math.atan2(FLX, FLY);
             BRA = Math.atan2(BRX, BRY);
             BLA = Math.atan2(BLX, BLY);
 
+            //set the target speeds based on the target X and Y speeds
             FRT = Math.abs(FRX)+Math.abs(FRY);
             FLT = Math.abs(FLX)+Math.abs(FLY);
             BRT = Math.abs(BRX)+Math.abs(BRY);
             BLT = Math.abs(BLX)+Math.abs(BLY);
 
+            //ensure none of the modules are expected to go a speed > 1.0
             highestSpeed = Math.max(Math.max(FRT, FLT), Math.max(BRT, Math.max(BLT, 1)));
-
             FRT = (Math.abs(FRX)+Math.abs(FRY)) / highestSpeed;
             FLT = (Math.abs(FLX)+Math.abs(FLY)) / highestSpeed;
             BRT = (Math.abs(BRX)+Math.abs(BRY)) / highestSpeed;
             BLT = (Math.abs(BLX)+Math.abs(BLY)) / highestSpeed;
 
+            //Limit the drive motor acceleration
             FRTOutput += Functions.Clamp(FRT-FRTOutput, -Constants.swerveModuleMaxThrottleChange, Constants.swerveModuleMaxThrottleChange);
             FLTOutput += Functions.Clamp(FLT-FLTOutput, -Constants.swerveModuleMaxThrottleChange, Constants.swerveModuleMaxThrottleChange);
             BRTOutput += Functions.Clamp(BRT-BRTOutput, -Constants.swerveModuleMaxThrottleChange, Constants.swerveModuleMaxThrottleChange);
